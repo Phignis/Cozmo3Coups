3a198765cc7da34b408fd67ecfc873f252e0f3eb
MANM-ANMM <mrouault@hotmail.fr>
2022-02-09 09:58:57 +0100

    Continuation du chi fou mi (ajout d'animation notament)

--- src/chifoumi.py
+++ src/chifoumi.py
@@ -6,7 +6,7 @@ import asyncio.tasks
 import face_images
 
 from cozmo.util import distance_mm, speed_mmps
-
+from random import randint
 import os
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
@@ -29,7 +29,7 @@ def find_someone_to_play(robot: cozmo.robot.Robot):
 		return None
 
 def move_lift_and_say(robot: cozmo.robot.Robot, nomMovement):
-	robot.set_lift_height(1).wait_for_completed()
+	robot.set_lift_height(0.9).wait_for_completed()
 
 	moveSay = robot.say_text(nomMovement, in_parallel=True, duration_scalar=0.8)
 	time.sleep(0.5)
@@ -42,29 +42,65 @@ def movement_chifoumi(robot: cozmo.robot.Robot):
 	move_lift_and_say(robot, "Fou")
 	move_lift_and_say(robot, "Mi")
 
+def displayCoupCozmo(robot: cozmo.robot.Robot, coupCozmo, duration):
+	if coupCozmo==0:
+		img = images["Chi"]
+	elif coupCozmo==1:
+		img = images["Fou"]
+	else:
+		img = images["Mi"]
+	return robot.display_oled_face_image(img, duration, in_parallel=True)
+
+
+class DetectionCoupJoueur:
+	def wait_for_completed(self):
+		pass
+
+	def getResult(self):
+		return randint(0,2)
+
+def detectJoueurCoup(robot, tete):
+	return DetectionCoupJoueur()
+
+def resultatManche(coup1, coup2):
+	dif = coup1-coup2
+	if (dif == 0):
+		return 0
+	elif (dif>0):
+		return 1
+	else:
+		return -1 
 
 
 def chifoumi(robot: cozmo.robot.Robot):
 	robot.enable_stop_on_cliff(True)
 	robot.drive_straight(distance_mm(100), speed_mmps(50)).wait_for_completed()
 
-
-	if (not find_someone_to_play(robot)):
+	teteJoueur = find_someone_to_play(robot)
+	if (not teteJoueur):
 		return
+
+	robot.play_anim_trigger(cozmo.anim.Triggers.RequestGameMemoryMatchAccept0).wait_for_completed()
 
 
 	movement_chifoumi(robot)
 
+	coupCozmo = randint(0,2)
+	robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE, in_parallel=True)
+	detectionCoupJoueur = detectJoueurCoup(robot, teteJoueur)
+	displayCoupC = displayCoupCozmo(robot, coupCozmo, 2000)
+	detectionCoupJoueur.wait_for_completed()
+	displayCoupC.wait_for_completed()
+
+	coupJoueur = detectionCoupJoueur.getResult()
 	
-	robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE).wait_for_completed()
-	robot.display_oled_face_image(images["Chi"], 3000).wait_for_completed()
-	robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE).wait_for_completed()
-	robot.display_oled_face_image(images["Fou"], 3000).wait_for_completed()
-	robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE).wait_for_completed()
-	robot.display_oled_face_image(images["Mi"], 3000).wait_for_completed()
+	resultat = resultatManche(coupCozmo, coupJoueur)
+	if resultat == -1:
+		robot.play_anim_trigger(cozmo.anim.Triggers.CubePounceWinRound).wait_for_completed()
+	elif resultat == 1:
+		robot.play_anim_trigger(cozmo.anim.Triggers.CubePounceLoseRound).wait_for_completed()
+	else:
+		robot.play_anim_trigger(cozmo.anim.Triggers.DizzyShakeLoop).wait_for_completed()
 
 
-
-
-
-cozmo.run_program(chifoumi)
+cozmo.run_program(chifoumi, use_viewer=True)
