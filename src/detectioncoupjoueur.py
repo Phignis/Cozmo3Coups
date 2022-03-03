462831031990e4b2e3f5bce9e857058d9c0ede8b
Phignis <baptiste63.foucras.pro@gmail.com>
2022-03-03 10:34:01 +0100

    correction des erreurs dans detectioncoupjoueur.p

--- src/detectioncoupjoueur.py
+++ src/detectioncoupjoueur.py
@@ -1,5 +1,5 @@
-
 import cozmo
+
 from random import randint
 
 
@@ -7,24 +7,22 @@ class DetectionCoupJoueurRandom:
 
 	def __init__(self, robot):
 		self.robot = robot
-		self.tete = tete
 
 	def wait_for_completed(self):
 		pass
 
 	def getResult(self):
-		return randint(0,2)
+		return randint(0, 2)
 
 
 class DetectionCoupJoueurCube:
 	def __init__(self, robot, cubes):
 		self.robot = robot
-		self.tete = tete
+		self.cubes = cubes
 
 	def wait_for_completed(self):
 		pass
 
 	def getResult(self):
-		return randint(0,2)
-	robot.world.wait_for(cozmo.objects.EvtObjectTapped)
+		return randint(0, 2)
 
